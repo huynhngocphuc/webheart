ce35193674eafb733d2de9cc2fd0b56672d0c64b
huynh ngoc phuc <huynhngocphuc.9d@gmail.com>
2022-10-11 10:03:53 +0700

    fix audio

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
 var imgWidth = 120; // width of images (unit: px)
 var imgHeight = 170; // height of images (unit: px)
-
+document.addEventListener('click', musicPlay);
+function musicPlay() {
+    document.getElementById('player').play();
+    document.removeEventListener('click', musicPlay);
+}
 // Link of background music - set 'null' if you dont want to play background music
 // var bgMusicURL =
 //   "https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a";
@@ -90,14 +94,14 @@ if (autoRotate) {
 }
 
 // add background music
-if (bgMusicURL) {
-  document.getElementById("music-container").innerHTML += `
-<audio src="${bgMusicURL}" ${bgMusicControls ? "controls" : ""
-    } autoplay loop>    
-<p>If you are reading this, it is because your browser does not support the audio element.</p>
-</audio>
-`;
-}
+// if (bgMusicURL) {
+//   document.getElementById("music-container").innerHTML += `
+// <audio src="${bgMusicURL}" ${bgMusicControls ? "controls" : ""
+//     } autoplay loop>    
+// <p>If you are reading this, it is because your browser does not support the audio element.</p>
+// </audio>
+// `;
+// }
 
 // setup events
 document.onpointerdown = function (e) {
