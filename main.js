a4637a4370b0d3e255fc93d7de714bf12325efd2
huynh ngoc phuc <huynhngocphuc.9d@gmail.com>
2022-10-11 09:40:07 +0700

    fix name

--- main.js
+++ main.js
@@ -9,10 +9,16 @@ var imgWidth = 120; // width of images (unit: px)
 var imgHeight = 170; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
-var bgMusicURL =
-  "https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a";
-var bgMusicControls = true; // Show UI music control
-
+// var bgMusicURL =
+//   "https://api.soundcloud.com/tracks/143041228/stream?client_id=587aa2d384f7333a886010d5f52f302a";
+// var bgMusicControls = true; // Show UI music control
+// var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+// if (!isChrome) {
+//   $('#iframeAudio').remove()
+// }
+// else {
+//   $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+// }
 /*
      NOTE:
        + imgWidth, imgHeight will work for video
@@ -86,9 +92,8 @@ if (autoRotate) {
 // add background music
 if (bgMusicURL) {
   document.getElementById("music-container").innerHTML += `
-<audio src="${bgMusicURL}" ${
-    bgMusicControls ? "controls" : ""
-  } autoplay loop>    
+<audio src="${bgMusicURL}" ${bgMusicControls ? "controls" : ""
+    } autoplay loop>    
 <p>If you are reading this, it is because your browser does not support the audio element.</p>
 </audio>
 `;
